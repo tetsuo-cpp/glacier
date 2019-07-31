721f5a88fa04a285d17e4ebfd659f5f591b7702f
Alex Cameron <alex.cameron@10gen.com>
2019-07-31 20:46:58 +1000

    Add helper for stack impl.

--- vm/Stack.c
+++ vm/Stack.c
@@ -1,23 +1,31 @@
 #include "Stack.h"
 
+#include <stdbool.h>
+
+static inline bool glacierStackIsValidPointer(int pointer) {
+  return (pointer >= 0 && pointer < MAX_STACK_SIZE);
+}
+
 void glacierStackInit(GlacierStack *stack) { stack->stackPointer = 0; }
 
 int glacierStackPush(GlacierStack *stack, int value) {
-  if (stack->stackPointer >= MAX_STACK_SIZE)
+  if (!glacierStackIsValidPointer(stack->stackPointer))
     return -1;
   stack->data[stack->stackPointer++].value = value;
   return 0;
 }
 
 int glacierStackTop(GlacierStack *stack, int *val) {
-  if (stack->stackPointer - 1 < 0)
+  int headPointer = stack->stackPointer - 1;
+  if (!glacierStackIsValidPointer(headPointer))
     return -1;
-  *val = stack->data[stack->stackPointer - 1].value;
+  *val = stack->data[headPointer].value;
   return 0;
 }
 
 int glacierStackPop(GlacierStack *stack, int *value) {
-  if (stack->stackPointer - 1 < 0)
+  int headPointer = stack->stackPointer - 1;
+  if (!glacierStackIsValidPointer(headPointer))
     return -1;
   if (value)
     glacierStackTop(stack, value);
