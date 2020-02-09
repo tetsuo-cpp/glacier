35009c85dde088fa423bfc8f44f9c7524b7e5fe7
Alex Cameron <ascottcameron@gmail.com>
2020-02-09 21:06:33 +1100

    Begin rebuilding the hash map once we hit the maximum depth.

--- vm/ds/Map.c
+++ vm/ds/Map.c
@@ -12,12 +12,13 @@ struct GlacierMapNode {
 };
 
 #define GLC_MAP_INIT_BUCKET_LEN 2
+#define GLC_MAP_MAX_DEPTH 3
 
 static size_t glacierMapHash(GlacierValue value);
+static int glacierMapRebuild(GlacierMap *map);
 
 int glacierMapInit(GlacierMap *map) {
-  GLC_RET(glacierCAlloc(GLC_MAP_INIT_BUCKET_LEN,
-                        sizeof(GLC_MAP_INIT_BUCKET_LEN),
+  GLC_RET(glacierCAlloc(GLC_MAP_INIT_BUCKET_LEN, sizeof(GlacierMapNode),
                         (char **)&map->buckets));
   map->numBuckets = GLC_MAP_INIT_BUCKET_LEN;
   return GLC_OK;
@@ -25,18 +26,20 @@ int glacierMapInit(GlacierMap *map) {
 
 int glacierMapSet(GlacierMap *map, GlacierValue key, GlacierValue value) {
   size_t index = glacierMapHash(key) % map->numBuckets;
-  if (index >= map->numBuckets) {
-    // Grow buckets.
-    return GLC_OUT_OF_BUFFER;
-  }
   GlacierMapNode *node = &map->buckets[index];
+  unsigned int depth = 0;
   if (node->set) {
     GlacierMapNode *current = node;
     while (current->next != NULL)
-      current = current->next;
+      current = current->next, ++depth;
     assert(current->set);
     GLC_RET(glacierMAlloc(sizeof(GlacierMapNode), (char **)&current->next));
     node = current->next;
+  }
+  if (depth >= GLC_MAP_MAX_DEPTH) {
+    GLC_RET(glacierMapRebuild(map));
+    GLC_RET(glacierMapSet(map, key, value));
+    return GLC_OK;
   }
   node->key = key;
   node->value = value;
@@ -65,4 +68,25 @@ void glacierMapDestroy(GlacierMap *map) {
 size_t glacierMapHash(GlacierValue value) {
   (void)value;
   return 0;
+}
+
+int glacierMapRebuild(GlacierMap *map) {
+  int ret = GLC_OK;
+  GlacierMap newMap;
+  newMap.buckets = NULL;
+  newMap.numBuckets = map->numBuckets * 2;
+  GLC_RET(glacierCAlloc(newMap.numBuckets, sizeof(GlacierMapNode),
+                        (char **)&newMap.buckets));
+  for (size_t i = 0; i < map->numBuckets; ++i) {
+    GlacierMapNode *current = &map->buckets[i];
+    while (current != NULL && current->set) {
+      GLC_ERR(glacierMapSet(&newMap, current->key, current->value));
+      current = current->next;
+    }
+  }
+  glacierMapDestroy(map);
+  *map = newMap;
+err:
+  glacierMapDestroy(&newMap);
+  return ret;
 }
