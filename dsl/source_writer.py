e1f6a3d1c5dbb421c138f5c5946ecc6b4797ecba
Alex Cameron <ascottcameron@gmail.com>
2020-07-06 22:09:21 +1000

    Refactor opcode helper generation to use SourceWriter utility class.

--- dsl/source_writer.py
+++ dsl/source_writer.py
@@ -3,20 +3,21 @@ INDENT_WIDTH = 4
 
 class SourceWriter:
     def __init__(self):
-        self.indent = 0
+        self.indent_level = 0
         self.source = str()
 
     def indent(self):
-        self.indent += INDENT_WIDTH
+        self.indent_level += INDENT_WIDTH
 
     def unindent(self):
-        self.indent -= INDENT_WIDTH
+        self.indent_level -= INDENT_WIDTH
+        assert self.indent_level >= 0
 
     def reset_indent(self):
-        self.indent = 0
+        self.indent_level = 0
 
     def write_line(self, line):
-        self.source += " " * self.indent + line + "\n"
+        self.source += " " * self.indent_level + line + "\n"
 
     def get_source(self):
         return self.source
